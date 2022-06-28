3a4d7542cca72ca75d70c1ba5d3c779ddefc634c
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-06-28 23:49:56 +0300

    hw23Start

---
+++ src/test/java/com/example/hw23/Hw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

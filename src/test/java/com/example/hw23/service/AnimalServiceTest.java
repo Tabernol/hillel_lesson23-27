f375564686664e72fde5706dfcd271ff5f02ffd7
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-07-07 22:55:17 +0300

    hw24v3

--- src/test/java/com/example/hw23/service/AnimalServiceTest.java
+++ src/test/java/com/example/hw23/service/AnimalServiceTest.java
@@ -1,36 +1,6 @@
 package com.example.hw23.service;
 
-import com.example.hw23.entity.Animal;
-import com.example.hw23.repository.AnimalRepository;
-import com.example.hw23.repository.AnimalRepositoryImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalServiceTest {
-//
-//    //    private final AnimalRepositoryI animalRepository = Mockito.mock(AnimalRepository.class);
-////
-////    private final AnimalRepository spy = Mockito.spy(animalRepository);
-//    @Autowired
-//   // private AnimalRepositoryImpl animalRepository;
-//    private final AnimalService animalServ = new AnimalService(new AnimalRepositoryImpl());
-//
-//    @Test
-//    public void saveAnimalTest() {
-//        Animal animal = new Animal(1, "mouse", 2);
-//        assertEquals("mouse", animalServ.save(animal).getName());
-//    }
-//
-//
-////    @Test
-////    void get() {
-////        Animal animal = new Animal(1, "sheep", 2);
-////        assertEquals(animal, animalService.get(1));
-////
-////    }
+
 }

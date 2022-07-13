75413198e1d7bc1a46f291c410fc6c8bd7c37f5a
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-07-13 22:56:41 +0300

    hw27test

--- src/main/java/com/example/hw23/config/MyStudyAuthenticationConfig.java
+++ src/main/java/com/example/hw23/config/MyStudyAuthenticationConfig.java
@@ -1,33 +1,33 @@
-package com.example.hw23.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
-
-
-@Configuration
-@EnableWebSecurity
-public class MyStudyAuthenticationConfig extends WebSecurityConfigurerAdapter {
-
-    private final DataSource dataSource;
-
-    public MyStudyAuthenticationConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("Max")
-                .password("{noop}1111")
-                .authorities("ROLE_ADMIN")
-                .and()
-                .withUser("Smith")
-                .password("{noop}hammer")
-                .authorities("ROLE_USER");
-    }
-}
+//package com.example.hw23.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//import javax.sql.DataSource;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class MyStudyAuthenticationConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final DataSource dataSource;
+//
+//    public MyStudyAuthenticationConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder
+//                .inMemoryAuthentication()
+//                .withUser("Max")
+//                .password("{noop}1111")
+//                .authorities("ROLE_ADMIN")
+//                .and()
+//                .withUser("Smith")
+//                .password("{noop}hammer")
+//                .authorities("ROLE_USER");
+//    }
+//}
